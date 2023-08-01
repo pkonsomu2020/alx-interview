0dfdcb8f20d50445de626243c48f4a16dc8da5c8
pkonsomu2020 <pkonsomu2020@gmail.com>
2023-08-01 01:47:34 -0700

    Updated task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,15 +3,22 @@
 Module that determines if all the boxes can be opened.
 """
 
+
 def canUnlockAll(boxes):
-    n = len(boxes)
-    visited = [False] * n
-    stack = [0]  # Initialize the stack with the first box (index 0)
+    """
+    Prototype that determines if all the boxes can be opened.
+    """
+    size = len(boxes)  # size of the list of boxes.
+    checkbox = {}  # dictionary that will contain the boxes that can be opened.
+    index = 0  # index of the box that will be checked.
 
-    while stack:
-        box_idx = stack.pop()
-        if not visited[box_idx]:
-            visited[box_idx] = True
-            stack.extend(boxes[box_idx])
-
-    return all(visited)
+    for keys in boxes:  # for each box in the list of boxes.
+        if len(keys) == 0 or index == 0:
+            checkbox[index] = -1
+        for key in keys:
+            if key < size and key != index:
+                checkbox[key] = key  # the box is added to the dictionary.
+        if len(checkbox) == size:
+            return True  # all the boxes can be opened.
+        index += 1
+    return False
