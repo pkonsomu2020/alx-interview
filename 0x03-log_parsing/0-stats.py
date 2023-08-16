abc6f8b6089d690310e267ec5e20a7734e254336
pkonsomu2020 <pkonsomu2020@gmail.com>
2023-08-16 01:24:43 -0700

    Final Push

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
-import signal
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
 
-def print_stats(signal, frame):
-    print("File size:", total_size)
-    for code in sorted(status_codes):
-        print("{}: {}".format(code, status_codes[code]))
-    sys.stdout.flush()
-
-signal.signal(signal.SIGINT, print_stats)
-
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-line_count = 0
+counter = 0
 
-for line in sys.stdin:
-    line_parts = line.split()
-    if len(line_parts) == 9:
-        try:
-            status_code = int(line_parts[7])
-            file_size = int(line_parts[8])
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-            total_size += file_size
-            line_count += 1
-        except ValueError:
-            pass
-    if line_count == 10:
-        print_stats(None, None)
-        line_count = 0
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
