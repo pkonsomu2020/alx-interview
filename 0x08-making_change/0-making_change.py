e0e59528c61e4d4bccae448798440d16516a243f
pkonsomu2020 <pkonsomu2020@gmail.com>
2023-09-19 11:33:40 +0300

    Updated Tasks

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,22 +2,22 @@
 """given a pile of coins of different values determine the fewest coins"""
 
 def makeChange(coins, total):
-    # Create a list to store the minimum number of coins needed for each total
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # It takes 0 coins to make a total of 0
+    """determine the fewest number of coins"""
+    if total <= 0:
+        return 0
 
-    # Iterate through each coin value
-    for coin in coins:
-        # Update dp for each total amount from coin value to the target total
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+    coins.sort(reverse=True)
 
-    # Check if it's possible to make the target total
-    if dp[total] == float('inf'):
-        return -1
-    else:
-        return dp[total]
+    i, n_coins = (0, 0)
+    cpy_total = total
+    len_coins = len(coins)
 
-# Test cases
-print(makeChange([1, 2, 25], 37))  # Output: 7
-print(makeChange([1256, 54, 48, 16, 102], 1453))  # Output: -1
+    while(i < len_coins and cpy_total > 0):
+        if (cpy_total - coins[i]) >= 0:
+            cpy_total -= coins[i]
+            n_coins += 1
+        else:
+            i += 1
+
+    check = cpy_total > 0 and n_coins > 0
+    return -1 if check or n_coins == 0 else n_coins
